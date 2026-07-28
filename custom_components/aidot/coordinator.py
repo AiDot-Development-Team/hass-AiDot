7936ec000ca18fdfeee192b2ae7243b74bb9b844
Bryan <hongjinbin@leedarson.com>
2026-07-28 11:23:41 +0800

    Sync AiDot integration with Home Assistant Core

--- custom_components/aidot/coordinator.py
+++ custom_components/aidot/coordinator.py
@@ -5,13 +5,19 @@ import logging
 from typing import override
 
 from aidot.client import AidotClient
-from aidot.const import CONF_ACCESS_TOKEN
+from aidot.const import (
+    CONF_ACCESS_TOKEN,
+    CONF_AES_KEY,
+    CONF_DEVICE_LIST,
+    CONF_ID,
+    CONF_TYPE,
+)
 from aidot.device_client import DeviceClient, DeviceStatusData
 from aidot.exceptions import AidotAuthFailed, AidotUserOrPassIncorrect
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import ConfigEntryAuthFailed
+from homeassistant.exceptions import ConfigEntryError
 from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
@@ -89,15 +95,24 @@ class AidotDeviceManagerCoordinator(DataUpdateCoordinator[None]):
         try:
             await self.async_auto_login()
         except AidotUserOrPassIncorrect as error:
-            raise ConfigEntryAuthFailed from error
+            raise ConfigEntryError from error
 
     @override
     async def _async_update_data(self) -> None:
         """Update data async."""
         try:
-            current_devices = await self.client.async_get_all_device()
+            data = await self.client.async_get_all_device()
         except AidotAuthFailed as error:
-            raise ConfigEntryAuthFailed from error
+            raise ConfigEntryError from error
+        current_devices = {
+            device[CONF_ID]: device
+            for device in data[CONF_DEVICE_LIST]
+            if (
+                device[CONF_TYPE] == "light"
+                and CONF_AES_KEY in device
+                and device[CONF_AES_KEY][0] is not None
+            )
+        }
 
         removed_ids = set(self.device_coordinators) - set(current_devices)
         for dev_id in removed_ids:
@@ -124,7 +139,7 @@ class AidotDeviceManagerCoordinator(DataUpdateCoordinator[None]):
     def token_fresh_cb(self) -> None:
         """Update token."""
         self.hass.config_entries.async_update_entry(
-            self.config_entry, data=self.client.login_info
+            self.config_entry, data=self.client.login_info.copy()
         )
 
     async def async_auto_login(self) -> None:
